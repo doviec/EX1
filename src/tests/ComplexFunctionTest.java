96ed48e369fdd28184f8e63fa86fc7cfd8582f71
doviec <doviechitiz@gmail.com>
2019-12-03 14:02:14 +0200

    test for tostring

--- src/tests/ComplexFunctionTest.java
+++ src/tests/ComplexFunctionTest.java
@@ -10,7 +10,7 @@ class ComplexFunctionTest {
 	
 
 	@Test
-	void ComplexFunction() {
+	void testComplexFunction() {
 		String s1 = "mul(1,2)"; 
 		String s2 = "mul(mul(1,2),3)";
 		String s3 = "mul(1,mul(2,3))";
@@ -19,9 +19,11 @@ class ComplexFunctionTest {
 		ComplexFunction complex2 = new ComplexFunction(s2);
 		ComplexFunction complex3 = new ComplexFunction(s3);
 		
+		System.out.println(s1.toString());
+		
 	}
 	@Test
-	void plus() {
+	void testPlus() {
 		
 		String s1 = "x^2";
 		String s2 = "x";
@@ -38,7 +40,7 @@ class ComplexFunctionTest {
 		complex2.plus(complex4);
 	}
 	@Test
-	void mul() {
+	void testMul() {
 		
 		String s1 = "x^2";
 		String s2 = "x";
@@ -55,7 +57,7 @@ class ComplexFunctionTest {
 		complex2.mul(complex4);
 	}
 	@Test
-	void div() {
+	void testDiv() {
 		
 		String s1 = "x^2";
 		String s2 = "x";
@@ -72,7 +74,7 @@ class ComplexFunctionTest {
 		complex2.div(complex4);
 	}
 	@Test
-	void max() {
+	void testMax() {
 		
 		String s1 = "x^2";
 		String s2 = "x";
@@ -89,7 +91,7 @@ class ComplexFunctionTest {
 		complex2.max(complex4);
 	}
 	@Test
-	void min() {
+	void testMin() {
 		
 		String s1 = "x^2";
 		String s2 = "x";
@@ -106,7 +108,7 @@ class ComplexFunctionTest {
 		complex2.min(complex4);
 	}
 	@Test
-	void comp() {
+	void testComp() {
 		
 		String s1 = "x^2";
 		String s2 = "x";
@@ -121,6 +123,20 @@ class ComplexFunctionTest {
 		complex1.comp(complex2);
 		complex3.comp(complex2);
 		complex2.comp(complex4);
+	}
+	@Test
+	void testToString() {
+		String s1 = "mul(1,2)"; 
+		String s2 = "mul(mul(1,2),3)";
+		String s3 = "mul(1,mul(2,3))";
+		
+		ComplexFunction complex1 = new ComplexFunction(s1);
+		ComplexFunction complex2 = new ComplexFunction(s2);
+		ComplexFunction complex3 = new ComplexFunction(s3);
+				
+		assertEquals("mul(1,2)",s1.toString());
+		assertEquals("mul(mul(1,2),3)",s2.toString());
+		assertEquals("mul(1,mul(2,3))",s3.toString());	
 	}
 
 }
